3275a92c526f0d55fa93d9b185a9571987f3924a
Hassan Mohamed <hassan.mhmd656@gmail.com>
2023-04-01 15:18:22 +0200

    format success response interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
+import { SucessResponseInterceptor } from './common/helper/sucess-response.interceptor';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
@@ -14,6 +16,12 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: SucessResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/common/helper/sucess-response.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class SucessResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          isSuccess: true,
+          message: 'success',
+          data,
+          errors: [],
+        };
+      }),
+    );
+  }
+}
